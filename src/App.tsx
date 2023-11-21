5ff88478ab6278bc28316cbef5d571a631fced89
Пользователь <you@example.com>
2023-11-21 10:44:37 +0300

    add text

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 function App() {
     return (
         <div className="App">
-            <Title>Portfolio Roman</Title>
+            <Title>Portfolio</Title>
         </div>
     );
 }

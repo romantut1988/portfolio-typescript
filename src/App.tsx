df4aa33b4a18f3d450f804bab115e7df88d4a5a9
Пользователь <you@example.com>
2023-11-22 14:22:49 +0300

    main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import {Header} from './layout/header/Header'
+import {Main} from './layout/section/main/Main'
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Main/>
         </div>
     );
 }
---
+++ src/layout/section/main/Main.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import photo from '../../../assets/images/i.jpg'
+import {FlexWrapper} from '../../../components/FlexWrapper';
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-around"}>
+                <div>
+                    <Name>Hi There</Name>
+                    <h2>I am Roman Amelkovich</h2>
+                    <MainTitle>A Web Developer.</MainTitle>
+                </div>
+
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div `
+    min-height: 100vh;
+  background-color: #fff5e7;
+`
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+`
+
+const MainTitle = styled.h1 `
+  
+`
+const Name = styled.h2 `
+  
+`

6f7ea4ee884e72c608e43796aa6870e67ccb027e
Roman <romantut1988@mail.ru>
2023-11-24 00:48:00 +0300

    section slogan structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Header} from './layout/header/Header'
 import {Contact} from './layout/section/contacts/Contacts';
 import {Main} from './layout/section/main/Main'
 import {Skills} from './layout/section/skills/Skills';
+import {Slogan} from './layout/section/slogan/Slogan';
 import {Testimony} from './layout/section/testimony/Testimony';
 import {Works} from './layout/section/works/Works';
 
@@ -15,6 +16,7 @@ function App() {
             <Works/>
             <Testimony/>
             <Contact/>
+            <Slogan/>
         </div>
     );
 }
---
+++ src/layout/section/slogan/Slogan.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Button} from '../../../components/Button';
+import {SectionTitle} from '../../../components/SectionTitle';
+
+export const Slogan = () => {
+    return (
+        <StyledSlogan>
+            <SectionTitle>I Am Available For Freelance</SectionTitle>
+            <Button>Hire me</Button>
+        </StyledSlogan>
+    );
+};
+
+const StyledSlogan = styled.section`
+  min-height: 30vh;
+  background-color: #ed2d14;
+`
+

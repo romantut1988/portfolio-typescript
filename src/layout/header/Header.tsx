2fca3c46419a2b6035f578094f7c3e31cf261b73
Roman <romantut1988@mail.ru>
2023-11-25 23:02:25 +0300

    global css styles

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import {Container} from '../../components/Container';
+import {FlexWrapper} from '../../components/FlexWrapper';
 import {Logo} from '../../components/logo/Logo';
 import {Menu} from '../../components/menu/Menu';
 
@@ -8,16 +10,18 @@ const items = ["Home", "Skills", "Works", "Testimony", "Contact"]
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu menuItems={items}/>
+            <Container>
+                <FlexWrapper justify={"space-between"} align={"center"}>
+                    <Logo/>
+                    <Menu menuItems={items}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
   background-color: #d4ffd3;
-  display: flex;
-  justify-content: space-between;
 `
 
 

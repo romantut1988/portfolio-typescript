0fae5eaf65136f70bdb8397a87b8805a38f9d7ce
Roman <romantut1988@mail.ru>
2023-11-27 00:21:54 +0300

    general indentation for sections

--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -34,6 +34,10 @@ export const GlobalStyle = createGlobalStyle`
     border: none;
   }
 
+  .section {
+    padding: 100px 0;
+  }
+  
   section:nth-of-type(odd) {
     background-color: ${theme.colors.primaryBg};
   }

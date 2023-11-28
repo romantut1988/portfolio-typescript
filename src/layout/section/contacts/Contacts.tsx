3119876c1383fb500c74b07c4a6856a6839eb087
Roman <romantut1988@mail.ru>
2023-11-28 12:30:16 +0300

    contacts section styles

--- src/layout/section/contacts/Contacts.tsx
+++ src/layout/section/contacts/Contacts.tsx
@@ -1,35 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Button} from '../../../components/Button';
+import {Container} from '../../../components/Container';
 import {SectionTitle} from '../../../components/SectionTitle';
+import {theme} from '../../../styles/Theme';
 
 export const Contact = () => {
     return (
         <StyledContacts>
-            <SectionTitle>Contact</SectionTitle>
-            <StyledForm>
-                <Field placeholder={"name"}/>
-                <Field placeholder={"subject"}/>
-                <Field placeholder={"message"} as={"textarea"}/>
-                <Button type={"submit"}>Send message</Button>
-            </StyledForm>
+            <Container>
+                <SectionTitle>Contact</SectionTitle>
+                <StyledForm>
+                    <Field placeholder={"name"}/>
+                    <Field placeholder={"subject"}/>
+                    <Field placeholder={"message"} as={"textarea"}/>
+                    <Button type={"submit"}>Send message</Button>
+                </StyledForm>
+            </Container>
         </StyledContacts>
     );
 };
 
 const StyledContacts = styled.section`
-  min-height: 50vh;
-  background-color: #efe9be;
 `
 const StyledForm = styled.form`
-  max-width: 500px;
+  max-width: 540px;
   width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  align-items: center;
+  gap: 16px;
   margin: 0 auto;
+
+  textarea {
+    resize: none;
+    height: 155px;
+  }
 `
 const Field = styled.input`
+  width: 100%;
+  background-color: ${theme.colors.secondaryBg};
+  border: ${theme.colors.borderColor};
+  padding: 7px 15px;
 
+  color: ${theme.colors.font};
+  font-family: Poppins, sans-serif;
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: 0.6px;
+  
+  &::placeholder {
+    color: ${theme.colors.placeholderColor};
+    text-transform: capitalize;
+  }
+  
+  &:focus-visible {
+    outline: 1px solid ${theme.colors.borderColor};
+  }
 `
 
--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -3,6 +3,9 @@ export const theme = {
         primaryBg: "#1F1F20",
         secondaryBg: "#252527",
         accent: "#7572D5",
-        font: "#FFFFFF"
+        font: "#FFFFFF",
+
+        borderColor: "#4A4A4A",
+        placeholderColor: "#495057"
     }
 }

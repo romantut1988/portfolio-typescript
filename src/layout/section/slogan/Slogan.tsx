1e8d368357326359c8a72e2b6355de138544372e
Roman <romantut1988@mail.ru>
2023-11-28 23:41:22 +0300

    slogan section styles

--- src/layout/section/slogan/Slogan.tsx
+++ src/layout/section/slogan/Slogan.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import {Button} from '../../../components/Button';
+import {Container} from '../../../components/Container';
+import { FlexWrapper } from '../../../components/FlexWrapper';
 import {SectionTitle} from '../../../components/SectionTitle';
 
 export const Slogan = () => {
     return (
         <StyledSlogan>
-            <SectionTitle>I Am Available For Freelance</SectionTitle>
-            <Button>Hire me</Button>
+            <Container>
+                <FlexWrapper direction={"column"} align={"center"}>
+                    <SectionTitle>I Am Available For Freelance</SectionTitle>
+                    <Button>Hire me</Button>
+                </FlexWrapper>
+            </Container>
         </StyledSlogan>
     );
 };
 
 const StyledSlogan = styled.section`
-  min-height: 30vh;
-  background-color: #ed2d14;
 `
 

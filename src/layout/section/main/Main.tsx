9fb00f3fed8c5246c001abd164c48953aa8b6bf2
Roman <romantut1988@mail.ru>
2023-12-04 13:41:28 +0300

    miskin for fonts

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -4,12 +4,13 @@ import photo from '../../../assets/images/i.jpg'
 import {Container} from '../../../components/Container';
 import {FlexWrapper} from '../../../components/FlexWrapper';
 import {theme} from '../../../styles/Theme';
+import { font } from '../../../styles/Common';
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
+                <FlexWrapper align={"center"} justify={"space-between"} wrap={"wrap"}>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Roman Amelkovich</span></Name>
@@ -34,10 +35,17 @@ const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+  
+  @media ${theme.media.mobile} {
+    width: 310px;
+    height: 380px;
+  }
 `
+
 const PhotoWrapper = styled.div`
   position: relative;
   z-index: 0;
+  margin-top: 65px;
   
   &::before {
     content: "";
@@ -48,18 +56,22 @@ const PhotoWrapper = styled.div`
     top: -24px;
     left: 24px;
     z-index: -1;
+    
+    @media ${theme.media.mobile} {
+      width: 314px;
+      height: 414px;
+    }
   }
 `
 const Name = styled.h2`
-  font-family: Josefin Sans, sans-serif;
-  font-size: 50px;
-  font-weight: 700;
-  letter-spacing: 2.5px;
+  ${font({ family: "Josefin Sans, sans-serif", weight: 700, Fmax: 50, Fmin: 36})}
+  letter-spacing: 0.05em;
   margin: 10px 0;
 
   span {
     position: relative;
     z-index: 0;
+    white-space: nowrap;
 
     &::before {
       position: absolute;
@@ -72,13 +84,17 @@ const Name = styled.h2`
       background-color: ${theme.colors.accent};
     }
   }
+  
+  @media ${theme.media.mobile} {
+    margin: 15px 0 22px;
+  }
+`
+const MainTitle = styled.h1`
+  ${font({weight: 400, Fmax: 27, Fmin: 20})}
 `
 const SmallText = styled.h2`
   font-size: 14px;
   font-weight: 400;
 `
-const MainTitle = styled.h1`
-  font-size: 27px;
-  font-weight: 400;
-`
+
 

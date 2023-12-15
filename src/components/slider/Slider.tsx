fc54c6fac9d64b239676301e2da79c4f0ad72633
Roman <romantut1988@mail.ru>
2023-12-15 12:40:23 +0300

    adding a slider to the testimony section

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,27 +1,55 @@
 import React from 'react';
-import styled from 'styled-components';
-import {theme} from '../../styles/Theme';
-import {FlexWrapper} from '../FlexWrapper';
-import {S} from './Slider_Styles';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
-export const Slider: React.FC = () => {
-    return (
-        <S.Slider>
-            <FlexWrapper>
-                <S.Slide>
-                    <S.Text>
-                        Roman Amelkovich. Programmer engineer, programming course teacher, technical specialist, mentor.
-                    </S.Text>
-                    <S.Name>Roman Amelkovich</S.Name>
-                </S.Slide>
-            </FlexWrapper>
-            <S.Pagination>
-                <span></span>
-                <span className={"active"}></span>
-                <span></span>
-            </S.Pagination>
-        </S.Slider>
-    );
+const responsive = {
+    0: {items: 1},
+    568: {items: 2},
+    1024: {items: 3},
 };
 
+const items = [
+    <div className="item" data-value="1">1</div>,
+    <div className="item" data-value="2">2</div>,
+    <div className="item" data-value="3">3</div>,
+    <div className="item" data-value="4">4</div>,
+    <div className="item" data-value="5">5</div>,
+];
 
+const Carousel = () => (
+    <AliceCarousel
+        mouseTracking
+        items={items}
+        responsive={responsive}
+        controlsStrategy="alternate"
+    />
+);
+
+
+// import React from 'react';
+// import styled from 'styled-components';
+// import {theme} from '../../styles/Theme';
+// import {FlexWrapper} from '../FlexWrapper';
+// import {S} from './Slider_Styles';
+//
+// export const Slider: React.FC = () => {
+//     return (
+//         <S.Slider>
+//             <FlexWrapper>
+//                 <S.Slide>
+//                     <S.Text>
+//                         Roman Amelkovich. Programmer engineer, programming course teacher, technical specialist, mentor.
+//                     </S.Text>
+//                     <S.Name>Roman Amelkovich</S.Name>
+//                 </S.Slide>
+//             </FlexWrapper>
+//             <S.Pagination>
+//                 <span></span>
+//                 <span className={"active"}></span>
+//                 <span></span>
+//             </S.Pagination>
+//         </S.Slider>
+//     );
+// };
+//
+//
